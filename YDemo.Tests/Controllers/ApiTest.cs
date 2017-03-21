e0da855778f538bf98f716501d4e003034e7f00a
DESKTOP-IEKEOIM\vi000 <vi000246@gmail.com>
2017-03-21 15:45:24 +0800

    開始重構

---
+++ YDemo.Tests/Controllers/ApiTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using YDemo.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using YDemo.Controllers;
+using System.Web.Http.Results;
+
+namespace YDemo.Tests.Controllers
+{
+    [TestClass]
+    public class ApiTest
+    {
+
+    }
+}
