5e030da6e97c34a9fca554ec1de495bfec92cc8b
DESKTOP-IEKEOIM\vi000 <vi000246@gmail.com>
2017-03-21 12:50:37 +0800

    增加movie API

---
+++ YDemo/Controllers/movieAPIController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using YDemo.Models;
+
+namespace YDemo.Controllers
+{
+    public class movieAPIController : ApiController
+    {
+        private MovieDBContext db = new MovieDBContext();
+
+        public IEnumerable<Movie> GetAllMovies()
+        {
+            return db.Movies;
+        }
+
+        public IHttpActionResult GetMovie(string Title)
+        {
+            var movie = db.Movies.FirstOrDefault((p) => p.Title == Title);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+            return Ok(movie);
+        }
+    }
+}
--- YDemo/Global.asax.cs
+++ YDemo/Global.asax.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Web.Http;
 
 namespace YDemo
 {
@@ -13,6 +14,8 @@ namespace YDemo
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            GlobalConfiguration.Configuration.Formatters.XmlFormatter.SupportedMediaTypes.Clear();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
